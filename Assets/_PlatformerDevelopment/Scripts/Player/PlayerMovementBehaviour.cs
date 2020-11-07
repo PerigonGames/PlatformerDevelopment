676bf6973f41bad6a98dbf3651dc52f699b7763d
Martin <m4rtin.t@gmail.com>
2020-11-07 11:23:30 -0500

    Adding in Character bindings

---
+++ Assets/_PlatformerDevelopment/Scripts/Player/PlayerMovementBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PersonalDevelopment
+{
+    public class PlayerMovementBehaviour : MonoBehaviour
+    {
+        private PlayerCharacterBindings _bindings = null;
+
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+            _bindings = new PlayerCharacterBindings();
+            _bindings.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _bindings.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            print("On Move Pressed");
+        }
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            print("On Fire");
+        }
+
+        public void OnMelee(InputAction.CallbackContext context)
+        {
+            print("On Melee");
+        }
+    }
+}
