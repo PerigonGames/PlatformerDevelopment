9b5d27d37282ae5e39a56c4d15b6b2aed115999a
Martin <m4rtin.t@gmail.com>
2020-11-07 09:24:55 -0500

    Added new scene and connected it

--- Assets/_PlatformerDevelopment/Scripts/Views/PlayerJoinScreenBehaviour.cs
+++ Assets/_PlatformerDevelopment/Scripts/Views/PlayerJoinScreenBehaviour.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 namespace PersonalDevelopment
 {
     public class PlayerJoinScreenBehaviour : MonoBehaviour
     {
-    
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                StateManager.Instance.SetState(State.PreGame);
+                SceneManager.LoadScene(Scene.Map.ToString(), LoadSceneMode.Additive);
+            }
+        }
     }
 }
 
