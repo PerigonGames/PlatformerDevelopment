2e1433677a8c0bd241c4925fcba7d49fdffe3b81
Michael Goh <mikegoh1997@gmail.com>
2021-02-28 13:44:42 -0800

    Created build script for TeamCity to use

---
+++ Assets/Editor/BuildPlayerExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Build Windows")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/_PlatformerDevelopment/Scenes/Menu.unity" };
+        buildPlayerOptions.locationPathName = "C:/Users/Michael/Documents/windows/windowsBuild.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
